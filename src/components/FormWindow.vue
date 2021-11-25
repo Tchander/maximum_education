820d37316cb14e0256ac4583da4f888b503effaa
Tchander <evgeniy.tchander@yandex.ru>
2021-11-25 13:57:40 +0300

    feat: add title and form borders

---
+++ src/components/FormWindow.vue
@@ -0,0 +1,16 @@
+<template>
+  <form class="mxed-form"></form>
+</template>
+
+<script>
+export default {
+  name: "FormWindow",
+};
+</script>
+
+<style lang="scss" scoped>
+.mxed-form {
+  border: 1px solid #e1e1e1;
+  border-radius: 4px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="home"></div>
+  <div class="mxed-container">
+    <h3 class="mxed__title">Форма подачи заявки в отдел сервиса и качества</h3>
+    <form-window />
+  </div>
 </template>
 
 <script>
+import FormWindow from "../components/FormWindow";
 export default {
   name: "Home",
+  components: { FormWindow },
 };
 </script>
+
+<style lang="scss">
+.mxed-container {
+  width: 100%;
+  max-width: 920px;
+  margin: 0 auto;
+  .mxed__title {
+    color: #4e4a49;
+    font-weight: 700;
+    font-size: 20px;
+    letter-spacing: 0.5px;
+    margin: 12px 0 24px 0;
+  }
+}
+</style>
