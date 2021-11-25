a9a094c7c6a52a28f0008cdb6c37e58e7ef5f55d
Tchander <evgeniy.tchander@yandex.ru>
2021-11-25 14:28:42 +0300

    feat: install axios

---
+++ src/components/BranchSelector.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="mxed__label">Ваш филиал</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BranchSelector"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/const.js
@@ -0,0 +1,2 @@
+export const CITIES_URL =
+  "https://6196084574c1bd00176c6ff1.mockapi.io/api/v1/cities";
