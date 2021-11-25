5c098c75ac647d0a1217e82930d6f8d17123d760
Tchander <evgeniy.tchander@yandex.ru>
2021-11-25 14:01:00 +0300

    ref: transfer title from Home to FormWindow component

--- src/components/FormWindow.vue
+++ src/components/FormWindow.vue
@@ -1,5 +1,8 @@
 <template>
-  <form class="mxed-form"></form>
+  <div>
+    <h3 class="mxed__title">Форма подачи заявки в отдел сервиса и качества</h3>
+    <form class="mxed-form"></form>
+  </div>
 </template>
 
 <script>
@@ -9,6 +12,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.mxed__title {
+  color: #4e4a49;
+  font-weight: 700;
+  font-size: 20px;
+  letter-spacing: 0.5px;
+  margin: 12px 0 24px 0;
+}
 .mxed-form {
   border: 1px solid #e1e1e1;
   border-radius: 4px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="mxed-container">
-    <h3 class="mxed__title">Форма подачи заявки в отдел сервиса и качества</h3>
     <form-window />
   </div>
 </template>
@@ -18,12 +17,5 @@ export default {
   width: 100%;
   max-width: 920px;
   margin: 0 auto;
-  .mxed__title {
-    color: #4e4a49;
-    font-weight: 700;
-    font-size: 20px;
-    letter-spacing: 0.5px;
-    margin: 12px 0 24px 0;
-  }
 }
 </style>
