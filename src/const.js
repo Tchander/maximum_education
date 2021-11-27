c24349db3349764cfd610dad991b9bc2ff830532
Tchander <evgeniy.tchander@yandex.ru>
2021-11-27 20:24:53 +0300

    feat: create successful window component and add it to the routes

--- src/const.js
+++ src/const.js
@@ -1,3 +1,8 @@
+export const ROUTES = {
+  home: "/",
+  success: "/success",
+};
+
 export const CITIES_URL =
   "https://6196084574c1bd00176c6ff1.mockapi.io/api/v1/cities";
 
---
+++ src/views/SuccessfulWindow.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>42</div>
+</template>
+
+<script>
+export default {
+  name: "SuccessfulWindow",
+};
+</script>
+
+<style lang="scss" scoped></style>
